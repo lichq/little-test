c12272d65035a77a9e5e247864716ba628c8551b
lichq <34657125+lichq@users.noreply.github.com>
2022-07-22 15:59:08 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -21,19 +21,35 @@ def scale(Intrin_Mat, Extrin_Mat, height, width, filename):
             # print(idx)
             [u, v, w] = np.dot(inv_Intrin_Mat, idx) * dep
             point  = np.array([ u, v , w, 1])
-            [x, y, z, _] = np.dot(inv_Extrin_Mat, point)
+            [x, y, z, _] = np.dot(Extrin_Mat, point)
+            if x < 0:
+                print(x)
             # print(x, y)
             # x = x % 5
             # y = y % 5
-            if -0.5 < x < 0.5 and -0.5   < y < 0.5 :
+            while(x >= 2.5):
+                x = x - 5
+            while (x <= -2.5):
+                x = x + 5
+            while(y >= 2.5):
+                y = y - 5
+            while (y <= -2.5):
+                y = y + 5
+            if -0.5 < x < 0.5 and -0.5 < y < 0.5 :
                 img[i][j] = 210
             else:
                 [u, v, w] = np.dot(inv_Intrin_Mat, idx) * (dep+5)
                 point = np.array([ u, v , w, 1])
-                [x, y, z, _] = np.dot(inv_Extrin_Mat, point)
+                [x, y, z, _] = np.dot(Extrin_Mat, point)
                 # print(z)
-                # x = x % 5
-                # y = y % 5
+                while(x >= 2.5):
+                    x = x - 5
+                while (x <= -2.5):
+                    x = x + 5
+                while(y >= 2.5):
+                    y = y - 5
+                while (y <= -2.5):
+                    y = y + 5
                 if -2  < x < 2   and -2  < y < 2 :
                     img[i][j] = 128
                 else:
@@ -44,14 +60,14 @@ def scale(Intrin_Mat, Extrin_Mat, height, width, filename):
     # im.save(filename)   
     im.show(filename)
 
-Extrin_Mat = np.array([[1, 0, 0,3],
-                                    [0,1,0, 3],
-                                    [0, 0, 1, 0],
-                                    [0, 0, 0, 1]])
+Extrin_Mat = np.array([[1, 0, 0, 3.5],
+                        [0,1,0, 3.5],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]])
 
 Intrin_Mat = np.array([[1000, 0, 256],
-                                    [0, 1000, 256],
-                                    [0, 0, 1]])
+                        [0, 1000, 256],
+                        [0, 0, 1]])
 
 # Extrin_Mat = mattranspose(Extrin_Mat)
 
