f7991215c53e14bef3999d525bb988b174d04500
lichq <34657125+lichq@users.noreply.github.com>
2022-07-31 15:01:46 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -2,12 +2,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 from PIL import Image
-
-def mattranspose(mat):
-    mat[0][-1] = mat[0][-1] % 5
-    mat[1][-1] = mat[1][-1] % 5
-    print(mat)
-    return mat
     
 def scale(Intrin_Mat, Extrin_Mat, height, width, filename):
     inv_Intrin_Mat = np.linalg.inv(Intrin_Mat)
@@ -22,8 +16,6 @@ def scale(Intrin_Mat, Extrin_Mat, height, width, filename):
             [u, v, w] = np.dot(inv_Intrin_Mat, idx) * dep
             point  = np.array([ u, v , w, 1])
             [x, y, z, _] = np.dot(Extrin_Mat, point)
-            if x < 0:
-                print(x)
             # print(x, y)
             # x = x % 5
             # y = y % 5
@@ -57,11 +49,11 @@ def scale(Intrin_Mat, Extrin_Mat, height, width, filename):
 
     im = Image.fromarray(img)
     im = im.convert('L')
-    # im.save(filename)   
-    im.show(filename)
+    im.save(filename)   
+    # im.show(filename)
 
-Extrin_Mat = np.array([[1, 0, 0, 3.5],
-                        [0,1,0, 3.5],
+Extrin_Mat = np.array([[1, 0, 0, 0],
+                        [0,1,0, 0],
                         [0, 0, 1, 0],
                         [0, 0, 0, 1]])
 
@@ -69,9 +61,22 @@ Intrin_Mat = np.array([[1000, 0, 256],
                         [0, 1000, 256],
                         [0, 0, 1]])
 
-# Extrin_Mat = mattranspose(Extrin_Mat)
+fileHandler  =  open  ("data.txt",  "r")
+listOfLines  =  fileHandler.readlines()
+fileHandler.close()
 
-scale(Intrin_Mat, Extrin_Mat, 512, 512, "try111.png")
+num = 1
+for  line in  listOfLines:
+    print(num)
+    trans = line.strip()
+    trans = trans.split(', ')
+    for i in range(0, len(trans)):
+        trans[i] = int(trans[i])
+    tranT=[trans[i:i+4] for i in range(0,len(trans),4)]
+    tranT = np.array(tranT)
+    Extrin_Mat = np.dot(tranT, Extrin_Mat)
+    scale(Intrin_Mat, Extrin_Mat, 512, 512, "%03d.png" %num)
+    num = num + 1
 # for sc in np.arange(-0.1, 0.1, 0.01):
 #     movex = round(sc, 2)
 #     movey = round(movex + 0.05, 2)
